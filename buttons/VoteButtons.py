a73476c73463937bf5f9b5889960c93f08ec08a5
tankstertim <tankstertim@gmail.com>
2023-03-27 20:19:07 -0700

    hangman bugfixes

--- buttons/VoteButtons.py
+++ buttons/VoteButtons.py
@@ -5,7 +5,7 @@ import json
 
 class VoteInviteButtons(discord.ui.View):
   def __init__(self, client,game_type,game):
-    super().__init__(timeout=90)
+    super().__init__(timeout=30)
     self.value  = None
     self.game_type = game_type
     self.game = game
